c7b8aa5ddd94366b775fcf5faebcf73f123306c4
Xon <tazipper1@hotmail.com>
2018-01-09 09:15:54 +0800

    Initial automated migration

---
+++ upload/src/addons/SV/WhoReplied/XenForo/Pub/Controller/Thread.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SV\WhoReplied\XenForo\Pub\Controller;
+
+/*
+ * Extends \XenForo\Pub\Controller\Thread
+ */
+class Thread extends XFCP_Thread
+{
+}
---
+++ upload/src/addons/SV/WhoReplied/_output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\WhoReplied\XenForo\Pub\Controller
+{
+	class XFCP_Thread extends \XenForo\Pub\Controller\Thread {}
+}
