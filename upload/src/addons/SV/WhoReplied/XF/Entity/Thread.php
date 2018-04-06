5544419634d7fc05c950db45e1c43ae912b13e67
Xon <tazipper1@hotmail.com>
2018-04-07 06:28:43 +0800

    Formatting cleanup

--- upload/src/addons/SV/WhoReplied/XF/Entity/Thread.php
+++ upload/src/addons/SV/WhoReplied/XF/Entity/Thread.php
@@ -11,9 +11,11 @@ class Thread extends XFCP_Thread
      * @param string|null $error
      * @return bool
      */
-    public function canViewWhoReplied(/** @noinspection PhpUnusedParameterInspection */$error = null)
+    public function canViewWhoReplied(/** @noinspection PhpUnusedParameterInspection */
+        $error = null)
     {
         $visitor = \XF::visitor();
+
         return $visitor->hasNodePermission($this->node_id, 'whoRepliedView');
     }
 }
--- upload/src/addons/SV/WhoReplied/XF/Pub/Controller/Thread.php
+++ upload/src/addons/SV/WhoReplied/XF/Pub/Controller/Thread.php
@@ -10,65 +10,67 @@ use XF\Mvc\ParameterBag;
 
 class Thread extends XFCP_Thread
 {
-	public function actionWhoReplied(ParameterBag $params)
-	{
+    public function actionWhoReplied(ParameterBag $params)
+    {
         $threadId = $params->get('thread_id');
         /** @var \SV\WhoReplied\XF\Entity\Thread $thread */
         $thread = $this->assertViewableThread($threadId, $this->getThreadViewExtraWith());
 
-		if (!$thread->canViewWhoReplied()) {
-			return $this->noPermission();
-		}
+        if (!$thread->canViewWhoReplied())
+        {
+            return $this->noPermission();
+        }
 
-		$criteria = $this->filter('criteria', 'array');
+        $criteria = $this->filter('criteria', 'array');
         //$secondaryOrder = $this->filter('order', 'str');
         //$secondaryDirection = $this->filter('direction', 'str');
 
-		$page = isset($params['page']) ? $params['page'] : 1;
-		$perPage = \XF::options()['WhoReplied_usersPerPage'];
+        $page = isset($params['page']) ? $params['page'] : 1;
+        $perPage = \XF::options()['WhoReplied_usersPerPage'];
 
-		$filter = $this->filter('_xfFilter', [
-			'text' => 'str',
-			'prefix' => 'bool'
-		]);
-		$searcher = $this->searcher('XF:User', $criteria);
+        $filter = $this->filter('_xfFilter', [
+            'text'   => 'str',
+            'prefix' => 'bool'
+        ]);
+        $searcher = $this->searcher('XF:User', $criteria);
 
-		$finder = $searcher->getFinder();
+        $finder = $searcher->getFinder();
         $finder->with("ThreadUserPost|{$threadId}", true);
         $finder->order("ThreadUserPost|{$threadId}.post_count", 'DESC');
         $finder->order('user_id');
 
-		if (strlen($filter['text']))
-		{
-			$finder->where(
-				'username',
-				'LIKE',
-				$finder->escapeLike($filter['text'], $filter['prefix'] ? '?%' : '%?%')
-			);
-		}
-		$finder->limitByPage($page, $perPage);
+        if (strlen($filter['text']))
+        {
+            $finder->where(
+                'username',
+                'LIKE',
+                $finder->escapeLike($filter['text'], $filter['prefix'] ? '?%' : '%?%')
+            );
+        }
+        $finder->limitByPage($page, $perPage);
 
-		$total = $finder->total();
-		$users = $finder->fetch();
+        $total = $finder->total();
+        $users = $finder->fetch();
 
-		$this->assertValidPage($page, $perPage, $total, 'thread/who-replied');
+        $this->assertValidPage($page, $perPage, $total, 'thread/who-replied');
 
 
-		$viewParams = [
-			'thread' => $thread,
-			'forum' => $thread->Forum,
-			'users' => $users,
+        $viewParams = [
+            'thread' => $thread,
+            'forum'  => $thread->Forum,
+            'users'  => $users,
 
-			'total' => $total,
-			'page' => $page,
-			'perPage' => $perPage,
+            'total'   => $total,
+            'page'    => $page,
+            'perPage' => $perPage,
 
-			'criteria' => $searcher->getFilteredCriteria(),
-			'filter' => $filter['text'],
-			'sortOptions' => [],
-			'order' => '',
-			'direction' => ''
-		];
-		return $this->view('XF:Thread\WhoReplied', 'whoreplied_list', $viewParams);
-	}
+            'criteria'    => $searcher->getFilteredCriteria(),
+            'filter'      => $filter['text'],
+            'sortOptions' => [],
+            'order'       => '',
+            'direction'   => ''
+        ];
+
+        return $this->view('XF:Thread\WhoReplied', 'whoreplied_list', $viewParams);
+    }
 }
