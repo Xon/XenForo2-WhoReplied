634f47b33c20b388820acfd43aa9424da74717af
Xon <635541+Xon@users.noreply.github.com>
2025-08-17 14:30:21 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/WhoReplied/_output/option_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $svWhoRepliedPagination Who replied pagination features
+ * @property array{0: int, 1: int}|null $svWhoReplied_usersPerPageChoices Allowed "Users per page" choices
+ */
+class Options
+{
+}
